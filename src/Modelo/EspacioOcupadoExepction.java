59365379ac25d8eb58992791ff68e5c54b6eab41
joanrecioj <joamreciojimenez@gmail.com>
2026-08-22 00:31:59 -0600

    Se hace una exeption tipo EspacioOcupadoExeption

---
+++ src/Modelo/EspacioOcupadoExepction.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author joanreciojimenez
+ */
+public class EspacioOcupadoExepction extends Exception{
+    public EspacioOcupadoExepction(String mensaje){
+        super(mensaje);
+    }
+}
