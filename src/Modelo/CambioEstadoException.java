8f8418c491b986cf6210b928409b05ee76a5a634
BrandonXdev <brchevezac@est.utn.ac.cr>
2026-08-25 16:33:47 -0600

    Excepcion Cambio de estado

---
+++ src/Modelo/CambioEstadoException.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author brand
+ */
+public class CambioEstadoException  extends Exception {
+    public CambioEstadoException(String mensaje){
+        super(mensaje);
+    }
+    
+}
